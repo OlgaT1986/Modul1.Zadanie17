d6a4fb147f9abf1ea6327f1fe4c915b0f638ce8d
Ольга <evinn86@mail.ru>
2022-01-27 18:47:48 +0700

    Добавьте файлы проекта.

---
+++ Modul1.Zadanie17/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul1.Zadanie17
+{
+    class BankAccount<T>
+    {
+        // номер счета
+        public T NumberAccount { get; set; }  
+        // баланс счета
+        public int BalanceAccount { get; set; } 
+        // фио
+        public string Name { get; set; } 
+        // метод возвращает строку
+        public string GetInfo() 
+        {
+            return $"{NumberAccount} {BalanceAccount} {Name}";
+        }
+        // метод запрашивает значение
+        public void Input()  
+        {
+            Console.WriteLine("Введите номера счета в банке");
+            string stringNumber = Console.ReadLine();
+            // принимает тип и возвращает значение к нужному типу
+            NumberAccount = (T)Convert.ChangeType(stringNumber, typeof(T)); 
+            Console.WriteLine("Введите баланс счета в банке");
+            BalanceAccount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО владельца счета в банке");
+            Name = Console.ReadLine();
+        }
+    }
+}
+
---
+++ Modul1.Zadanie17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+
+namespace Modul1.Zadanie17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> bankAccount1 = new BankAccount<int>();
+            bankAccount1.Input();
+            Console.WriteLine(bankAccount1.GetInfo());
+            Console.WriteLine();
+            BankAccount<string> bankAccount2 = new BankAccount<string>();
+            bankAccount2.Input();
+            Console.WriteLine(bankAccount2.GetInfo());
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
+
